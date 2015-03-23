4fdc8b8b1a05f2d13b105002f195807ca2c0f2c1
KaliYang <cosmiclatte@ewhain.net>
2015-03-23 22:40:44 +0900

    Lab2_1 file is modified.
    The content of Lab2_2 is updated.

--- Lab2_1.cpp
+++ Lab2_1.cpp
@@ -2,39 +2,59 @@
 #include "Lab2_1.h"
 #define FRONT_LED_PIN 10
 #define REAR_LED_PIN 9
+#define DUTY_CYCLE	20//20ms
 
-boolean increasing = true;
-int front_led_value = 0;
+boolean done = true;
+
+void PWM_Write(int pin, int on_time, int on_time_perc)
+{
+	int on_time = DUTY_CYCLE*on_time_perc/100.0;
+	int off_time = DUTY_CYCLE - on_time;
+
+	digitalWrtie(pin, HIGH);
+	delay(on_time);
+
+	digitalWrtie(pin, LOW);
+	delay(off_time);
+}
+
+void myAnalogWrite(int pin, int percent, int time)
+{
+	int num_loops = time/DUTY_CYCLE;
+	int i;
+	for(i=0;i<num_loops;i++)
+	{
+		PWM_Write(pin, percent);
+	}
+}
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
 // Add your initialization code here
-	pinMode(FRONT_LED_PIN,OUTPUT);
 	pinMode(REAR_LED_PIN,OUTPUT);
 }
 
 void loop()
 {
-	analogWrite(FRONT_LED_PIN,front_led_value);
-	analogWrite(REAR_LED_PIN,255 - front_led_value);
-	delay(1000);
+	if (done == false){
+		int value = 100;
+		int value_interval = 5;
+		//(value/value_interval) = number of steps =20
+		int total_time = 10000;
 
-	if(increasing == true){
-		front_led_value++;
-	}
-	else{
-		front_led_value--;
-	}
+		int time_per_step = total_time/(value/value_interval);
 
-	if(front_led_value > 255){
-		front_led_value = 254;
-		increasing = false;
+		while(value>0)
+		{
+			myAnalogWrite(REAR_LED_PIN, value, time_per_step);
+			value-=value_interval;
+		}
+
+		done = true;
 	}
-	else if(front_led_value <0){
-		front_led_value =1;
-		increasing = treu;
-	}
+	else
+		digitalWrtie(REAR_LED_PIN,LOW);
 
 
 }
